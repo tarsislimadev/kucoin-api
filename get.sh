d01501c50fab7af0e65a1d53b7315c29a9337b91
Tarsis <br.tmvdl@gmail.com>
2023-11-06 11:17:33 -0300

    Updating core script files

--- get.sh
+++ get.sh
@@ -4,4 +4,6 @@ API_VERSION="${1}"
 
 API_PATH="${2}"
 
-curl -sL -X GET "https://api.kucoin.com/api/${API_VERSION}/${API_PATH}"
+API_QUERIES="${3}"
+
+curl -sL -X GET "https://api.kucoin.com/api/${API_VERSION}/${API_PATH}?${API_QUERIES}"
